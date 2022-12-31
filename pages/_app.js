c1efbe12448d4784ce491ca7a037fd6675e8ac48
yuhanas yulianto <yulianto.yuhanas@gmail.com>
2022-12-31 13:43:01 +0700

    just basic hero section

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Inter } from "@next/font/google";
+
+const fonttype = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={fonttype.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
